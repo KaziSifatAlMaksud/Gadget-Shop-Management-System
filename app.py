a4c22a043e1e65e9e16a30da77df6e50ef8e18e2
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2022-12-08 23:06:56 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flasksfa!"
+
+
+@app.route("/home")
+def home_page():
+    return render_template('index.html')
+
+@app.route("/about")
+def about_page():
+    return render_template('mithu.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1",port=5005)

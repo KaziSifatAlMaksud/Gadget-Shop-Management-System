29f40a21617d59f2b4dcb181fb4aefdba713a3b4
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2022-12-09 18:40:42 +0600

    acc1

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+
 @app.route("/")
-def home():
-    return "Hello, Flasksfa!"
-
-
-@app.route("/landing")
 def home_page():
     return render_template('index.html')
 

5681cc5e58e37a9e027b9019c5fa32336766c6da
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2023-01-02 01:17:10 +0600

    cart

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect, session
 import pymongo
+from bson.objectid import ObjectId
 from flask import *
 app = Flask(__name__)
 app.secret_key = "sifat"
@@ -13,6 +14,7 @@ contactMess = mydb["contact"]
 @app.before_request
 def before_request():
     g.user = None
+
     if 'user' in session:
         g.user = session['user']
 
@@ -20,27 +22,22 @@ def before_request():
 @app.route("/", methods=['GET', 'POST'])
 def home_page():
     prodct_arry = []
+    if 'product' not in session:
+        session['product'] = []
     for y in shopProduct.find():
         prodct_arry.append(y)
     l = len(prodct_arry)
+    cart_list = session['product']
     if request.method == "POST":
         form_data = request.form
-        prodct_model = form_data['pid']
-        x = shopProduct.find_one({"Model":prodct_model})
-        if x is not None:
-            p_type = x["Type"]
-            p_price = x["price"]
-            p_model = x["Model"]
-            p_processor =x["Processor"]
-            p_display = x["Display"]
-            p_ram = x["Ram"]
-            p_feat = x["Features"]
-            p_warrant = x["Warranty"]
-            p_image = x["image"]
-            p_descript = x["Description"]
-            print(p_display)
-        return render_template('quick_view.html', **locals())
-    return render_template('index.html',**locals())
+        pr_model = form_data['pid']
+        cart_list.append(pr_model)
+        session['product'] = cart_list
+        print(session)
+        len_product = len(session['product'])
+        print(len_product)
+
+    return render_template('index.html', **locals())
 
 @app.route("/<name>", methods=['GET', 'POST'])
 def home(name):
@@ -56,13 +53,20 @@ def home(name):
 
 @app.route("/about")
 def about_page():
+    print(session)
     return render_template('about.html')
 @app.route("/checkout")
 def checkout_page():
     return render_template('checkout.html')
 @app.route("/cart")
 def cart_page():
-    return render_template('cart.html')
+    prodct_arry = []
+    len_product = len(session['product'])
+    for y in range(len_product):
+        x = shopProduct.find_one({"_id": ObjectId(session['product'][y])})
+        prodct_arry.append(x)
+    l = len(prodct_arry)
+    return render_template('cart.html',**locals())
 
 @app.route("/order")
 def order_page():
@@ -89,9 +93,18 @@ def update_address_page():
 
 @app.route("/profile")
 def profile_page():
-    if 'email' in session:
-        email = session['email']
-        return render_template('profile.html', name=email)
+    if 'user' in session:
+        user = session['user']
+        for x in mycol.find({"email": user}):
+            user_name = x['name']
+            session['name'] = user_name
+            user_mobile = x['mobile']
+            user_email = x['email']
+            if x['address'] is not None:
+                user_address = x['address']
+                return render_template('profile.html', **locals())
+            else:
+                return render_template('profile.html', **locals())
     else:
         return '<p>Please login first</p>'
 
@@ -99,7 +112,9 @@ def profile_page():
 @app.route("/logout")
 def logout_page():
     if 'user' in session:
-        session.pop('email', None)
+        session.pop('user', None)
+        session.pop('name', None)
+        session.pop('product',None)
         return render_template('login.html',**locals());
     else:
         return '<p>user already logged out</p>'
@@ -107,6 +122,7 @@ def logout_page():
 @app.route("/update_profile",methods=['GET',"POST"])
 def update_profile_page():
     if request.method == "POST":
+
         if request.form["btn"] == "update now":
             form_data = request.form
             user_name = form_data["name"]
@@ -128,10 +144,8 @@ def login():
         form_data = request.form
         username = form_data["email"]
         password = form_data["pass"]
-        session['user'] = username
         for x in mycol.find({"email": username}):
             for y in mycol.find({"re_pass": password}):
-                logprofile = True
                 session['user'] = username
                 return redirect(url_for('profile_page'))
         message = "Username or password is incorrect"
@@ -152,6 +166,7 @@ def register():
         user_d["mobile"] = user_mobile
         user_d["pass"] = user_pass
         user_d["re_pass"] = user_re_pass
+        user_d["address"] = ""
         mycol.insert_one(user_d)
         sucess_mess = "Register Successfully"
         #return render_template("login.html", **locals())
@@ -222,6 +237,20 @@ def search():
         return render_template('search.html',**locals())
 @app.route("/quick_view",methods=["GET","POST"])
 def quick_view():
+    if request.args.get('id') is not None:
+        id = request.args.get('id')
+        x = shopProduct.find_one({"_id": ObjectId(id)})
+        P_id = x['_id']
+        p_type = x["Type"]
+        p_price = x["price"]
+        p_model = x["Model"]
+        p_processor = x["Processor"]
+        p_display = x["Display"]
+        p_ram = x["Ram"]
+        p_feat = x["Features"]
+        p_warrant = x["Warranty"]
+        p_image = x["image"]
+        p_descript = x["Description"]
     return render_template('quick_view.html',**locals())
 
 if __name__ == "__main__":
